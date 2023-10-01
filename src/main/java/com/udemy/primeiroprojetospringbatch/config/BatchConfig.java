40b045c611b7d01e2754f8f1a58a6e3ddfea1594
AliceMeneses <alicesilvademeneses@gmail.com>
2023-10-01 10:06:51 -0300

    Permitiu multiplas execuções do mesmo Job graças ao autoincremento automático de um id como parâmetro para cada execução do mesmo Job

--- src/main/java/com/udemy/primeiroprojetospringbatch/config/BatchConfig.java
+++ src/main/java/com/udemy/primeiroprojetospringbatch/config/BatchConfig.java
@@ -4,6 +4,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +18,7 @@ public class BatchConfig {
         return jobBuilderFactory
                 .get("imprimeOla")
                 .start(step)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
